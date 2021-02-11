2eaf2badeb324c11422e13e29f447952dfaeb9fb
DustiasTheGuy <isakwebdev@gmail.com>
2021-02-11 19:27:52 +0100

    joqwjutoqwjt

--- server/main/main.go
+++ server/main/main.go
@@ -7,14 +7,20 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
+func apiMiddleware(c *fiber.Ctx) error {
+	return c.Next()
+}
+
 func main() {
 	app := fiber.New()
+	app.Static("/", "./public")
 	app.Use(cors.New())
 
-	apiGroup := app.Group("/api", func(c *fiber.Ctx) error {
-		return c.Next()
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendFile("index.html")
 	})
 
+	apiGroup := app.Group("/api", apiMiddleware)
 	apiGroup.Post("/new", api.NewPostHandler)
 	apiGroup.Get("/posts", api.ReadPostsHandler)
 
